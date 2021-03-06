c3d13c84cec7e5c294bff54a445e8098b81cb91a
Jasmine Steen <jasmine.steen.wrk@gmail.com>
2021-03-06 15:08:57 +0100

    getting started

---
+++ WpfPocketsphinxTest/AddPhraseWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfPocketsphinxTest
+{
+    /// <summary>
+    /// Interaction logic for AddPhraseWindow.xaml
+    /// </summary>
+    public partial class AddPhraseWindow : Window
+    {
+        public AddPhraseWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddPhraseBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
